bd1cdb0f3dd38985423fd7e690e623a2e36899b3
Carlos W. Mercado <carlos.washington.mercado@gmail.com>
2024-01-13 18:02:23 -0300

    finished prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //Console.WriteLine("Hello Prep1 World!");
+
+        Console.Write("What is your first name? ");
+        string name =  Console.ReadLine(); //Scott
+
+        Console.Write("What is your last name? ");
+        string surname = Console.ReadLine(); //Burton
+
+        Console.Write($"Your name is {surname}, {name} {surname}.");
     }
 }
