ae9d14a95838f68328b314574c4ebe027914d70d
Carlos W. Mercado <carlos.washington.mercado@gmail.com>
2024-02-21 22:42:11 -0300

    l04 finished version

---
+++ prepare/Learning04/MathAssignment.cs
@@ -0,0 +1,19 @@
+public class MathAssignment : Assignment
+{
+
+	private string _textbookSection;
+	private string _problems;
+
+	public MathAssignment(string textbookSection, string problems, string studentName, string topic) : base(studentName, topic)
+	{
+		_textbookSection = textbookSection;
+		_problems = problems;
+	}
+
+
+	public string GetHomeworklist()
+	{
+		return $"Section {_textbookSection} Problems {_problems}";
+	}
+
+}
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Console.WriteLine("Hello Learning04 World!");
+
+        System.Console.WriteLine();
+        var assignment = new Assignment("Samuel Bennett", "Multiplication");
+        System.Console.WriteLine(assignment.GetSummary());
+        
+        System.Console.WriteLine();
+        var math = new MathAssignment("7.3", "8-19", "Roberto Rodriguez", "Fractions");
+        System.Console.WriteLine(math.GetSummary());
+        System.Console.WriteLine(math.GetHomeworklist());
+        
+        System.Console.WriteLine();
+        var writing = new WritingAssignment("The Causes of World War II", "Mary Waters", "European History");
+        System.Console.WriteLine(writing.GetSummary());
+        System.Console.WriteLine(writing.GetWritingInformation());
     }
 }
