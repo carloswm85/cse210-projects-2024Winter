fee99e7bfe24b0844c609354dee97438dd90e0dd
Carlos W. Mercado <carlos.washington.mercado@gmail.com>
2024-02-21 22:46:33 -0300

    L04 improved version using teacher solution

--- prepare/Learning04/Assignment.cs
+++ prepare/Learning04/Assignment.cs
@@ -2,6 +2,16 @@ public class Assignment {
 	protected string _studentName;
 	private string _topic;
 
+	public string GetStudentName()
+	{
+		return _studentName;
+	}
+
+	public string GetTopic()
+	{
+		return _topic;
+	}
+
 	public Assignment(string studentName, string topic) {
 		_studentName = studentName;
 		_topic = topic;
--- prepare/Learning04/WritingAssignment.cs
+++ prepare/Learning04/WritingAssignment.cs
@@ -3,13 +3,15 @@ public class WritingAssignment : Assignment
 
 	private string _title;
 
-	public WritingAssignment(string title, string studentName, string topic) : base(studentName, topic)
+	public WritingAssignment(string title, string studentName, string topic)
+		: base(studentName, topic)
 	{
 		_title = title;
 	}
 
 	public string GetWritingInformation()
 	{
+		// var studentName = GetStudentName(); // Use in case this member variable is private
 		return $"{_title} by {_studentName}";
 	}
 
